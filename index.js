6f40fba049475ac32150678bcb15eaf8f0d9e22f
leejoneshane <leejoneshane@gmail.com>
2019-01-07 16:46:02 +0800

    update index.js

--- index.js
+++ index.js
@@ -36,6 +36,32 @@ app.get('/iceconfig', function (req, resp) {
   })
 })
 
-app.listen('80', function () {
+app.post('/iceconfig', function (req, resp) {
+    var query = req.query
+    var key = '4080218913'
+    var time_to_live = 600
+    var timestamp = Math.floor(Date.now() / 1000) + time_to_live
+    var turn_username = timestamp + ':gobert'
+    var password = hmac(key, turn_username)
+  
+    resp.setHeader("Access-Control-Allow-Origin", "*");   //设置跨域访问
+    
+    return resp.send({
+      iceServers: [
+        {
+          urls: [
+            'turn:localhost:3478?transport=udp',
+            'turn:localhost:3478?transport=tcp',
+            'turn:localhost:3479?transport=udp',
+            'turn:localhost:3479?transport=tcp'
+          ],
+          username: turn_username,
+          credential: password
+        }
+      ]
+    })
+})
+  
+app.listen('3033', function () {
   console.log('server started')
 })
