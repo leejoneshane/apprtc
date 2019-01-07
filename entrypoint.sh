6efbe151fa6fdbd75591a3f0e0bc0b2d0c729299
Sean Lee <leejoneshane@gmail.com>
2019-01-07 16:44:30 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,5 @@ exec dev_appserver.py --host=0.0.0.0 --port=80 ./out/app_engine
 exec /usr/src/app/go/bin/collidermain -port=8089 -tls=false -room-server="http://${HOSTNAME}"
 
 exec /usr/src/app/coturn/bin/turnserver --no-auth
+
+exec node /usr/src/app/rest/index.js
